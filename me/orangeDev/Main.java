9bb123a80c04fa5e254819af3ec10cde51fa987e
Fred <@>
2023-05-03 12:04:05 +0200

    Fast File download :)

--- me/orangeDev/Main.java
+++ me/orangeDev/Main.java
@@ -28,8 +28,11 @@ import me.orangeDev.utils.terminal.ColorUtil;
 import me.orangeDev.utils.terminal.LogUtil;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
 
@@ -105,34 +108,50 @@ public class Main {
                     if (input1.matches("[0-9]+")) {
                         int length = Integer.parseInt(input1);
                         if (length > 0) {
-                            Downloader.download("https://raw.githubusercontent.com/kkrypt0nn/Wordlists/master/famous/facebook_phished.txt", "facebook_phished.txt");
-                            Downloader.download("https://raw.githubusercontent.com/kkrypt0nn/Wordlists/master/famous/fasttrack.txt", "fasttrack.txt");
-                            Downloader.download("https://raw.githubusercontent.com/kkrypt0nn/Wordlists/master/famous/fern_wifi.txt", "fern_wifi.txt");
-                            Downloader.download("https://raw.githubusercontent.com/scipag/password-list/main/organizations/password-list-gov.txt", "password-list-gov.txt");
-                            Downloader.download("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt", "10-million-password-list-top-1000000.txt");
-                            Downloader.download("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10k-most-common.txt", "10k-most-common.txt");
-                            Downloader.download("https://raw.githubusercontent.com/berandal666/Passwords/master/rockyou-75.txt", "rockyou-75.txt");
-                            File facebook_phished = new File("facebook_phished.txt");
-                            File fasttrack = new File("fasttrack.txt");
-                            File fern_wifi = new File("fern_wifi.txt");
-                            File passwordListGov = new File("password-list-gov.txt");
-                            File tenMillion = new File("10-million-password-list-top-1000000.txt");
-                            File tenK = new File("10k-most-common.txt");
-                            File rocku75 = new File("rockyou-75.txt");
+                            CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
+                                Downloader.download("https://raw.githubusercontent.com/kkrypt0nn/Wordlists/master/famous/facebook_phished.txt", "facebook_phished.txt");
+                            });
+                            CompletableFuture<Void> future2 = CompletableFuture.runAsync(() -> {
+                                Downloader.download("https://raw.githubusercontent.com/kkrypt0nn/Wordlists/master/famous/fasttrack.txt", "fasttrack.txt");
+                            });
+                            CompletableFuture<Void> future3 = CompletableFuture.runAsync(() -> {
+                                Downloader.download("https://raw.githubusercontent.com/kkrypt0nn/Wordlists/master/famous/fern_wifi.txt", "fern_wifi.txt");
+                            });
+                            CompletableFuture<Void> future4 = CompletableFuture.runAsync(() -> {
+                                Downloader.download("https://raw.githubusercontent.com/scipag/password-list/main/organizations/password-list-gov.txt", "password-list-gov.txt");
+                            });
+                            CompletableFuture<Void> future5 = CompletableFuture.runAsync(() -> {
+                                Downloader.download("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt", "10-million-password-list-top-1000000.txt");
+                            });
+                            CompletableFuture<Void> future6 = CompletableFuture.runAsync(() -> {
+                                Downloader.download("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10k-most-common.txt", "10k-most-common.txt");
+                            });
+                            CompletableFuture<Void> future7 = CompletableFuture.runAsync(() -> {
+                                Downloader.download("https://raw.githubusercontent.com/berandal666/Passwords/master/rockyou-75.txt", "rockyou-75.txt");
+                            });
+
+                            CompletableFuture.allOf(future1, future2, future3, future4, future5, future6, future7).join();
+
                             ClearTerminalUtil.clear();
                             String generated = Generator.generatePassword(length);
                             LogUtil.otherLog(ColorUtil.ANSI_CYAN, "Generated Password", generated);
-                            System.out.println("");
-                            System.out.println("");
-                            System.out.println("");
+                            System.out.println("\n\n\n");
                             LogUtil.infoLog("Check if Password can be found in lists...");
-                            Checker.fileCheck(facebook_phished, generated);
-                            Checker.fileCheck(fasttrack, generated);
-                            Checker.fileCheck(fern_wifi, generated);
-                            Checker.fileCheck(passwordListGov, generated);
-                            Checker.fileCheck(tenMillion, generated);
-                            Checker.fileCheck(tenK, generated);
-                            Checker.fileCheck(rocku75, generated);
+
+                            List<File> files = Arrays.asList(
+                                    new File("facebook_phished.txt"),
+                                    new File("fasttrack.txt"),
+                                    new File("fern_wifi.txt"),
+                                    new File("password-list-gov.txt"),
+                                    new File("10-million-password-list-top-1000000.txt"),
+                                    new File("10k-most-common.txt"),
+                                    new File("rockyou-75.txt")
+                            );
+
+                            files.parallelStream().forEach(file -> {
+                                Checker.fileCheck(file, generated);
+                            });
+
                         }
                     }
                 }
